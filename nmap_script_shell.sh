23c3dcaa658756454624c63badf189e448cb4dbe
meepmaster <67827146+meepmaster360@users.noreply.github.com>
2022-05-04 02:13:51 +0100

    update
    Nice.
    
    Cheers, Meep.

--- nmap_script_shell.sh
+++ nmap_script_shell.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+
 clear
-echo -n "Which network do you wanna scan: "
+
+# Color variables 
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+# Nmap installation	
+	if [ ! -x "$(command -v nmap)" ];then
+        echo -e "\n${RED}[+]${NOCOLOR} nmap not detected...Installing\n"
+        sudo apt-get install nmap -y > installing;rm installing
+	else
+    echo -e "${GREEN}[+]${NOCOLOR} nmap detected\n"
+     
+	fi
+
+echo -e "${GREEN}Which network do you wanna scan: ${NOCOLOR}"
 read network
-echo -n "Which open port do you want discover:"
+
+echo -e "${GREEN}Which open port do you want discover:${NOCOLOR}"
 read port
 
-echo "searching in network $network port $port ..."
+echo -e "${GREEN}searching in network $network port $port ...${NOCOLOR}"
 
 nmap -sS -vv -p $port $network
